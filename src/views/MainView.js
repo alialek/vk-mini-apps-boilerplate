ef65d94d8ca15199310a2c699cdbb6d8343a5def
Просто Алек <alialek@yandex.ru>
2021-02-13 22:51:48 +0300

    Добавить функционал SwipeBack

--- src/views/MainView.js
+++ src/views/MainView.js
@@ -1,5 +1,5 @@
 import MainPanel from "../panels/MainPanel";
-
+import { withRouter } from '@happysanta/router';
 import React, { Component } from "react";
 import { View } from "@vkontakte/vkui";
 import {PANEL_MAIN, PANEL_QUIZ} from "./../router";
@@ -7,12 +7,15 @@ import QuizPanel from "../panels/QuizPanel";
 
 class Main extends Component {
   render() {
+    const { location, router } = this.props;
     return (
       <View
         id={this.props.id}
         popout={this.props.popout}
         modal={this.props.modal}
         activePanel={this.props.activePanel}
+        onSwipeBack={() => router.popPage()}
+				history={location.hasOverlay() ? [] : location.getViewHistory(VIEW_MAIN)}
       >
         <MainPanel id={PANEL_MAIN} />
         <QuizPanel id={PANEL_QUIZ} />
@@ -21,4 +24,4 @@ class Main extends Component {
   }
 }
 
-export default Main;
+export default WithRouter(Main);
